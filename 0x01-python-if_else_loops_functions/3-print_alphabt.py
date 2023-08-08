1a7ef36888fea37bc444fe66f92a268e728c78c6
Shindou7 <sh.shindou@gmail.com>
2023-08-08 06:45:20 -0700

    python0

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end="")
