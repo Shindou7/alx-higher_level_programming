1301b9ccdaa77da652304e476950eeb6c80dc485
Shindou7 <sh.shindou@gmail.com>
2023-08-14 10:52:07 -0700

    python2

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,45 +3,40 @@
 #include "lists.h"
 
 /**
-  *is_palindrome - Checks if a singly linked list is a palindrome
-  *@head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: The head of the singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	listint_t *start = NULL, *end = NULL;
+	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
 
-    if (head == NULL)
-        return (0);
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	start = *head;
+	len = listint_len(start);
+	len_cyc = len * 2;
+	len_list = len_cyc - 2;
+	end = *head;
 
-    if (*head == NULL)
-        return (1);
-    
-    start = *head;
-    len = listint_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
-
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
-
-        len_list = len_list - 2;
-    }
-
-    return (1);
+	for (; i < len_cyc; i = i + 2)
+	{
+		if (start[i].n != end[len_list].n)
+			return (0);
+		len_list = len_list - 2;
+	}
+	return (1);
 }
 
 /**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  * Return: The specific
-  */
+ * get_nodeint_at_index - Gets a node from a linked list
+ * @head: The head of the linked list
+ * @index: The index to find in the linked list
+ * Return: The specific
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current = head;
@@ -53,15 +48,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		{
 			if (iter_times == index)
 				return (current);
-
 			current = current->next;
 			++iter_times;
 		}
 	}
-
 	return (NULL);
 }
-
 /**
   * slistint_len - Counts the number of element
   * @h: The linked
@@ -76,6 +68,5 @@ size_t listint_len(const listint_t *h)
 		++lenght;
 		h = h->next;
 	}
-
 	return (lenght);
 }
