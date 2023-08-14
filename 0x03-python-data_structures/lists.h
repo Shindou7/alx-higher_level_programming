c7ee616ae68a4503ef28f76f99c2568a1bc84fc1
Shindou7 <sh.shindou@gmail.com>
2023-08-14 11:10:29 -0700

    python2

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,6 +18,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int is_palindrome(listint_t **head);
-/**size_t listint_len(const listint_t *h);*/
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
