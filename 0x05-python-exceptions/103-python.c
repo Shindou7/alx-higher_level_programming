20e4ba75b67507d8fe619a16f29bf06ec082fd65
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-28 22:25:12 +0100

    Update 103-python.c

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -64,8 +64,8 @@ void print_python_list(PyObject *p)
 		printf("  [ERROR] Invalid List Object\n");
 		return;
 	}
-	printf("[*] Size of the Python List = %d\n", listLen);
-	printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
+	printf("[*] Size of the Python List = %ld\n", listLen);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < listLen; ++i)
 	{
@@ -90,7 +90,6 @@ void print_python_list(PyObject *p)
 void print_python_float(PyObject *p)
 {
 	PyFloatObject *Obj = (PyFloatObject *)p;
-	float Obj;
 	double value;
 
 	printf("[.] float object info\n");
