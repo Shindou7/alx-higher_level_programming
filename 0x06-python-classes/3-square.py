dd866a03fc0cac1358c93a6288c41d6bf879ea89
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-29 16:30:34 +0100

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" 3-square.py """
+
+class Square:
+    """  class square """
+
+    def __init__(self, size=0) -> None:
+        """ class init
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        area square
+        """
+        return self.__size ** 2
