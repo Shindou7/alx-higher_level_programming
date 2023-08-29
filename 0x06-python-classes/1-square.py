f1f9ce560f6b5653c26e0c46cdbb1740e4a2db82
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-29 17:48:43 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""1-square.py"""
+"""1-square.py""
+
 
 class Square:
-    """ Declares a square class """
-    def __init__(self, size):
-        """class shindou
-        """
+    """This is an empty class square"""
+    def __init__(self, size=None):
         self.__size = size
