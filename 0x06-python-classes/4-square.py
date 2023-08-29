237484e728007c8aebece7504d7c1e703d011d27
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-29 16:36:10 +0100

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"4-square.py"
+
+class Square:
+    """class Square
+    """
+
+    def __init__(self, size=0):
+        """square def init
+        """
+        self.size = size
+
+    def area(self):
+        """class area
+        """
+        return self.__size ** 2
+
+
+    @property
+    def size(self):
+        """class size self
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """class size self value
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise value("size must be >= 0")
+        else:
+            self.__size = value
