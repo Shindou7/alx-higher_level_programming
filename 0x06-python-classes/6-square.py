d1abf2212b93db92bb5ff35ce970c4cb5ee2d318
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-29 19:39:47 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,10 +10,9 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
 
-        if self.__check_tuple(position) is False \
-            or self.__check_indexes(position) is False \
-            or self.__check_integers(position) is False \
-            or self.__check_values(position) is False:
+        if not isinstance(position, tuple) or len(position) != 2 \
+                or not isinstance(position[0], int) or not isinstance(position[1], int) \
+                or position[0] < 0 or position[1] < 0:
              raise TypeError('position must be a tuple of 2 positive integers')
          self.size = size
          self.position = position
@@ -38,35 +37,16 @@ class Square:
     @position.setter
     def position(self, position):
       """class positive"""
-      if self.__check_tuple(position) is False \
-         or self.__check_indexes(position) is False \
-         or self.__check_integers(position) is False \
-         or self.__check_values(position) is False:
-          raise TypeError('position must be a tuple of 2 positive integers')
+      if not isinstance(position, tuple) or len(position) != 2 \
+                or not isinstance(position[0], int) or not isinstance(position[1], int) \
+                or position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
       self.__position = position
 
 
     def __check_tuple(self, position):
     """class tuple"""
         if type(position) is tuple:
-            return True
-        return False
-
-    def __check_indexes(self, position):
-    """class check indexes"""
-        if len(position) == 2:
-            return True
-        return False
-
-    def __check_integers(self, position):
-    """class check integers"""
-        if type(position[0]) is int and type(position[1]) is int:
-            return True
-        return False
-
-    def __check_values(self, position):
-    """class check value"""
-        if position[0] >= 0 and position[1] >= 0:
             return True
         return False
 
@@ -80,13 +60,8 @@ class Square:
             print()
             return None
 
-        if self.__position[1] > 0:
-            for i in range(self.__position[1]):
-                print('')
-        for j in range(1, self.area() + 1):
-            if j % self.__size == 1:
-                print('{:>{w}}'.format('#', w=self.__position[0] + 1), end='')
-            else:
-                print('#', end='')
-            if j % self.__size == 0 and j > 0:
-                print()
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(' ' * self.__position[0] + '#' * self.__size)
