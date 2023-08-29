eb0b075db05b50ba38ea68b3e6dfede697b87505
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-29 17:57:30 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+"""Task"""
+
+
 class Square:
-    """ Declares a square class
-    """
+    """This is a class"""
     def __init__(self, size=0):
-        """ The __init__ method initializes the size value of the square
-        """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
