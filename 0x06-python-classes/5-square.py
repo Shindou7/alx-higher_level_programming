16d44d66f2533a3d4b09fb3a086baeecd44babe0
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-08-29 16:40:34 +0100

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""5-square.py"""
+
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+        self.size = size
+
+
+
+    @property
+    def size(self):
+        """class size"""
+        return self.__size
+
+
+
+    @size.setter
+    def size(self, value):
+        """class size self value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def area(self):
+        """class area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """class print"""
+        for i in range(self.size):
+            print("#" * self.size)
+
+
+        if not self.size:
+            print()
