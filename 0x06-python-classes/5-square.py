22e642c5b33905c5e7d4653c481c78cae296ee27
Shindou7 <sh.shindou@gmail.com>
2023-08-29 11:17:48 -0700

    p

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,20 +2,16 @@
 """5-square.py"""
 
 
-
 class Square:
+
     """class square"""
     def __init__(self, size=0):
         self.size = size
-
-
 
     @property
     def size(self):
         """class size"""
         return self.__size
-
-
 
     @size.setter
     def size(self, value):
@@ -26,7 +22,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-
     def area(self):
         """class area"""
         return self.__size ** 2
@@ -35,7 +30,5 @@ class Square:
         """class print"""
         for i in range(self.size):
             print("#" * self.size)
-
-
         if not self.size:
             print()
