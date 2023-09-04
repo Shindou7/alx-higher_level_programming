dbfee9ae7be610b3dcb0a9761d78f2302dd1f9dd
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-04 21:58:05 +0100

    Create 101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
+            return False
+    return True
+
+def print_solution(board):
+    solution = []
+    for row, col in enumerate(board):
+        solution.append([row, col])
+    print(solution)
+
+def solve_n_queens(size):
+    if size < 4:
+        print("N must be at least 4")
+        sys.exit(2)
+
+    board = [-1] * size
+
+    def place_queen(row):
+        if row == size:
+            print_solution(board)
+            return
+        for col in range(size):
+            if is_safe(board, row, col):
+                board[row] = col
+                place_queen(row + 1)
+
+    place_queen(0)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        size = int(sys.argv[1])
+        if size < 4:
+            raise ValueError
+    except ValueError:
+        print("N must be a positive integer")
+        sys.exit(1)
+
+    solve_n_queens(size)
