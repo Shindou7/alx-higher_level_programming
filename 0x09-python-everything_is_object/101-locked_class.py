0417773afa035dc242cce4637d59b5b25b376f26
Shindou7 <sh.shindou@gmail.com>
2023-09-05 06:49:24 -0700

    python8

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+
+lock class
+
+"""
+
 
 class LockedClass:
-    """
-    locked_class
-    """
-    __slots__ = "first_name"
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
