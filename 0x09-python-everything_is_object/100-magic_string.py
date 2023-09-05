9e1e421cddbf1e6e99d03e3b8e23f2d1c7348137
Shindou7 <sh.shindou@gmail.com>
2023-09-05 06:30:04 -0700

    p

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(rec=[]):
-    rec += ["BestSchool"]
-    return ", ".join(rec)
--- 0x09-python-everything_is_object/19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(3line):
-    return 3line.copy()
