00c4b26643220222e1e06f4022bb1166c89c48dd
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-11 18:44:07 +0100

    Create 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 7-base_geometry.py
+"""
+
+
+class BaseGeometry:
+   """ Empty class """
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Method that recieves the value property
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
