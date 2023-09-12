5c0f5f930f98c99295c63ac98b0bc05755b7feb4
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-12 21:23:18 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,7 +9,8 @@ def pascal_triangle(n):
         row = [1]
         if triangle:
             last_row = triangle[-1]
-            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
             row.append(1)
         triangle.append(row)
 
