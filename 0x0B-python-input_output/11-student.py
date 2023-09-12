adb140718027eebc58b3a82d1d4b47e9a7011ac0
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-12 20:46:42 +0100

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module 11-student.py
+that defines the class Student
+"""
+
+
+class Student():
+    """
+    Class to create student instances
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Special method to initialize
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        if attrs is None:
+            return self.__dict__
+        else:
+            directory = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    directory[att] = self.__dict__[att]
+            return directory
+
+    def reload_from_json(self, json):
+        for n in json:
+            if n in self.__dict__.keys():
+                self.__dict__[n] = json[n]
