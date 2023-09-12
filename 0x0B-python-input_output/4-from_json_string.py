25eb227b4f28318fc2cd5c6184283fe58d7f81e6
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-12 20:14:46 +0100

    Create 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string.py
+function that returns an objec JSON representation
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ 
+    returns an object by a JSON representation
+    """
+    return json.loads(my_str)
