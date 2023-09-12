42dbb5901f0a93e785346dc5898b3ef96b9aca55
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-09-12 20:53:17 +0100

    Create 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for i in range(1, len(prev_row)):
+            new_value = prev_row[i - 1] + prev_row[i]
+            new_row.append(new_value)
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
