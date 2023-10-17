6540089f52bbd2c7096ed32f10310d2ef4e0975e
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-17 20:27:32 +0100

    Create 14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Query to computes the score average of all records in the table second_table
+-- database name will be passed as an argument of the mysql command
+
+SELECT AVG(score) AS 'average' FROM second_table;
