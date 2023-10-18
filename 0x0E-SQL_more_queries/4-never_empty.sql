95e307ba56e283067ac473a9acd7dd4755ac6f09
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-18 21:19:01 +0100

    Update 4-never_empty.sql

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- should not fail if exists
+-- Query to create the table 'id_not_null' in MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256)
+);
