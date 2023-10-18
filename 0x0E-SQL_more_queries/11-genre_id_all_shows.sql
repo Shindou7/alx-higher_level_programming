54007af4539f0b37d8f2a1e9366e3203cbceb14b
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-18 21:03:35 +0100

    Update 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- the database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
