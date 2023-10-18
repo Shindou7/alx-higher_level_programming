be580ea1f986d57e94d3aee9e7b70a32174e0bfe
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-18 21:16:26 +0100

    Update 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- should not fail if either already exists
+-- Script that creates an user and a database in MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
