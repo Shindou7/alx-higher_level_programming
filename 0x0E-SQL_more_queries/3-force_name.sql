e46883eceb6e949e3c30dba673b288b33839ceef
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-18 21:17:40 +0100

    Update 3-force_name.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- database name will be passed as an argument of the mysql command
+-- Script that creates a table
+
+CREATE TABLE IF NOT EXISTS force_name (
+       id INT,
+       name VARCHAR(256) NOT NULL
+);
