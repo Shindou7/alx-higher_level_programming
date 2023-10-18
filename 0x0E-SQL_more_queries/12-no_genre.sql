14cd9ea98331db720d2be5f09ddbae0835848294
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-18 21:09:17 +0100

    Update 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- the database name will be passed as an argument of the mysql command
+-- Script that lists all cities contained in the database
+
+   SELECT tv_shows.title, tv_show_genres.genre_id
+     FROM tv_shows
+LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+ ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
