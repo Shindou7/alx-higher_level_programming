96bc9b2bdd1b2484b1a1e8d450b9feb411e3995a
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-10-18 21:00:46 +0100

    Update 103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+-- run this first to import a SQL dump -->
+-- Script that lists genres by their rating
+
+    SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
+      FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+  GROUP BY name
+  ORDER BY rating DESC;
