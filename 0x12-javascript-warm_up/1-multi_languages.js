222c94e1b0675cd6df6e5762287f9b54621a2fb6
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-11-13 18:48:23 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const data = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+data.forEach(line => console.log(line));
