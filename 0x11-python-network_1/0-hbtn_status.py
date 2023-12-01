9bf7dc538f879e19c05d43aa2ccf06298b43a78c
Shindou7 <sh.shindou@gmail.com>
2023-12-01 12:53:17 -0800

    gfd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,9 @@ from urllib.error import HTTPError
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        with urllib.request.urlopen(
+                'https://alx-intranet.hbtn.io/status'
+        ) as response:
             html = response.read()
             print('Body response:')
             print("\t- type: {}".format(type(html)))
