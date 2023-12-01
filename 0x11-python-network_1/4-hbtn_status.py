c0229aaced5d393742cf968e777e974be968a7e0
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-01 20:43:31 +0100

    Update 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+usage: ./4-hbtn_status.py | cat -e
+"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
